007119c70b473b8a3d92a7e98f845dcbe2c55def
wafiq <wafiqgithub775@gmail.com>
2024-09-10 23:16:15 +0600

    FEAT: add notification interface & service

---
+++ src/services/notification.service.ts
@@ -0,0 +1,78 @@
+import { INotificationDocument } from "@app/interfaces/notification.interface";
+import prisma from "../server/database"
+import { generateRandomId } from "@app/utils/common";
+
+export const createNotificationGroup = async (payload: INotificationDocument) => {
+    try {
+        const user = await prisma.notifications.create({
+            data: {
+                id: generateRandomId(),
+                ...payload
+            }
+        })
+        return user;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const getSingleNotificationGroup = async (notificationId: number) => {
+    try {
+        const user = await prisma.notifications.findFirst({
+            where: {
+                id: notificationId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+        return user;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const getAllNotificationGroupByUserId = async (userId: number) => {
+    try {
+        const user = await prisma.notifications.findMany({
+            where: {
+                userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+
+        return user;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const updateNotificationGroup = async (notificationId: number, payload: INotificationDocument) => {
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: notificationId
+            },
+            data: {
+                ...payload
+            }
+        })
+        return user;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const deleteNotificationGroup = async (notificationId: number) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: notificationId
+            }
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+}
